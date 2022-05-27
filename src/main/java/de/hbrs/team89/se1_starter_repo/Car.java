7b42d36d7f41ab86aa126ac42ea7406df54377d3
myrol <posthomerik@gmail.com>
2022-05-27 16:04:10 +0200

    Added more getter to Car + Refactoring

--- src/main/java/de/hbrs/team89/se1_starter_repo/Car.java
+++ src/main/java/de/hbrs/team89/se1_starter_repo/Car.java
@@ -2,6 +2,23 @@ package de.hbrs.team89.se1_starter_repo;
 
 import java.util.Arrays;
 
+/**
+ * params:
+ * [0] enter / leave
+ * [1] nr
+ * [2] Einfahrts-Zeit in sekunden nach 1970 (Unix Timestamp)
+ * [3] Dauer in Sekunden (impl. Ausfahrts-Zeit)
+ * [4] Preis
+ * [5] Ticket hash-Wert
+ * [6] Farbe des Autos
+ * [7] Parkplatz-Nr.
+ * [8] Geschlecht
+ * [9] Auto-Typ (PKW, SUV, ...)
+ * [10] Kennzeichen
+ * [11] = [2] ???
+ * [12] Parkhaus (Etage) (=NAME)
+ */
+
 public class Car implements CarIF {
     String[] params;
     public Car( String[] params ){
@@ -20,17 +37,34 @@ public class Car implements CarIF {
 
     @Override
     public long end() {
-        return Long.parseLong(params[2]);
+        // End = Begin (Unix Timestamp) + Duration (Seconds)
+        return Long.parseLong(params[1]) + Long.parseLong(params[2]);
     }
 
     @Override
     public int duration() {
-        return Integer.parseInt(params[3]);
+        return Integer.parseInt(params[2]);
     }
 
     @Override
     public int price() {
         return Integer.parseInt(params[4]);
+    }
+
+    public int space() {
+        return Integer.parseInt(params[7]);
+    }
+
+    public String gender() {
+        return params[8];
+    }
+
+    public String type() {
+        return params[9];
+    }
+
+    public String license() {
+        return params[10];
     }
 
     @Override
